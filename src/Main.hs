ad0fa6d9155475acadaf99ad4a598a49efe25997
Abhiroop Sarkar <asiamgenius@gmail.com>
2019-01-02 02:00:49 +0000

    Remove inefficient constructions

--- src/Main.hs
+++ src/Main.hs
@@ -3,23 +3,10 @@ module Main where
 
 import qualified Data.List as L
 
--- l = go 1000000 L.empty
---   where
---     go 0 t = L.cons 0 t
---     go n t = go (n - 1) (L.cons n t)
-
 l = go 10000000 L.empty
   where
     go 0 !t = L.cons 0 t
     go n !t = go (n - 1) (L.cons n t)
-
-foo = go 10 L.empty
-  where
-    go 0 t = t
-    go n t = go (n - 1) (L.conc (L.single n) t)
-
-
-
 
 
 len :: [Int] -> Int
